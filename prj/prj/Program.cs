af8416e61a514198032d3194214df82210393518
Yegor Popinako <yegor5x@gmail.com>
2022-09-17 12:05:20 +0300

    lab work

---
+++ prj/prj/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace prj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
